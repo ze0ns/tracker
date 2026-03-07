05c873119663cd00f10d418d7c73e04c66300295
zeons <oao@i-cio.ru>
2026-03-07 05:49:22 +0300

    Начало проекта

---
+++ Tracker/Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Oschepkov Aleksandr on 07.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
