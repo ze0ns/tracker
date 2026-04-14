283454b2b185ed7d25a3320f17c5fe2dbef1047a
zeons <oao@i-cio.ru>
2026-04-14 07:45:54 +0300

    Добавлен скриншот тест главного экрана ("экран пустой")

---
+++ Tracker/TrackerScreenShotTests/TrackerScreenShotTests.swift
@@ -0,0 +1,18 @@
+//
+//  TrackerScreenShotTests.swift
+//  TrackerScreenShotTests
+//
+//  Created by Oschepkov Aleksandr on 14.04.2026.
+//
+
+import Testing
+
+struct TrackerScreenShotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
